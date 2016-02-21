8536a48d9da6dcd9ca707427ffc281b1b02aea13
Rose Tucker <rosy533@gmail.com>
2016-02-21 15:40:10 +0000

    Separate dependancies into a build script

---
+++ installDependancies.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+brew reinstall nvm
+mkdir -p .nvm
+export NVM_DIR="$PWD/.nvm"
+source $(brew --prefix nvm)/nvm.sh
+nvm install 5.1.0
+rm -Rf "${TMPDIR}/jest_preprocess_cache"
+npm config set progress=false
+npm install -g mocha"
+npm install
+npm install -g flow-bin@`node -p "require('fs').readFileSync('.flowconfig', 'utf8').split('[version]')[1].trim()"`
