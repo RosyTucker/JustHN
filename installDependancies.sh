b041a435573d1251e471303d8fe31968fdbbd3a7
Rose Tucker <rosy533@gmail.com>
2016-02-21 15:42:55 +0000

    Remove random “

--- installDependancies.sh
+++ installDependancies.sh
@@ -6,6 +6,6 @@ source $(brew --prefix nvm)/nvm.sh
 nvm install 5.1.0
 rm -Rf "${TMPDIR}/jest_preprocess_cache"
 npm config set progress=false
-npm install -g mocha"
+npm install -g mocha
 npm install
 npm install -g flow-bin@`node -p "require('fs').readFileSync('.flowconfig', 'utf8').split('[version]')[1].trim()"`
